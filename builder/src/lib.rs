71502da1c9f8dc4e40e437ea8e2c6202ca4433da
Frank Taylor <frankt@vmware.com>
2024-05-03 14:12:12 +0100

    Step 1

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = syn::parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    quote! {}.into()
 }
