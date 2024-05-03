219a1eb9f5f31cd62b91410ce6eed42f8c5d5bec
Frank Taylor <frankt@vmware.com>
2024-05-03 14:48:09 +0100

    Step 2

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -6,5 +6,38 @@ use syn::DeriveInput;
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(input as DeriveInput);
 
-    quote! {}.into()
+    let name = &input.ident;
+    let bname = format!("{}Builder", name);
+    let bid = syn::Ident::new(&bname, name.span());
+
+    let fields = if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed { ref named, .. }),
+        ..
+    }) = input.data
+    {
+        named
+    } else {
+        unimplemented!("Can only build structs");
+    };
+
+    let builder_fields = fields.iter().map(|f| {
+        let name = &f.ident;
+        let ty = &f.ty;
+
+        quote! {
+            #name: std::option::Option<#ty>
+        }
+    });
+
+    let code = quote! {
+        pub struct #bid {
+           #(#builder_fields,)*
+        }
+
+        impl #name {
+            pub fn builder() -> () {
+            }
+        }
+    };
+    code.into()
 }
